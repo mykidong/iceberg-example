dc6ee96c7c1997a526d829c20ee6af18aaf2c43f
Kidong Lee <mykidong@gmail.com>
2022-12-10 16:24:05 +0900

    print envs

--- data-api/src/main/java/io/mykidong/iceberg/example/dataapi/controller/CollectController.java
+++ data-api/src/main/java/io/mykidong/iceberg/example/dataapi/controller/CollectController.java
@@ -94,6 +94,11 @@ public class CollectController  extends AbstractController implements Initializi
             endpoint = StringUtils.getEnv("S3_CREDENTIAL_ENDPOINT");
         }
         LOG.info("endpoint: {}", endpoint);
+
+
+        System.getenv().forEach((k, v) -> {
+            LOG.info(k + ":" + v);
+        });
     }
 
     public static Thread singletonEventLogToIceberg(DisruptorHttpReceiver receiver,
